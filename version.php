95b5d6632c54a7c9a9e111919798399e3c0f5f4c
MeganBohlandBylight <Megan.Bohland@Bylight.com>
2026-08-31 20:25:34 +0000

    chore(mod_cmi5): release 0.0.9
    Ticket: CCUI-3153
    
    git add version.php
    git commit -F- <<'EOF'
    chore(mod_cmi5): release 0.0.9
    
    Ticket: CCUI-3153

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_cmi5';
-$plugin->version = 2026082800;
+$plugin->version = 2026083100;
 $plugin->requires = 2022112800; // Moodle 4.1+.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.0.8';
+$plugin->release = '0.0.9';
